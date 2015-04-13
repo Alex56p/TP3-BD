0626e9d8e238bb9678834fe222db7e25787511f6
Alex56p <201332037@P104-06.clg.priv>
2015-04-13 14:20:31 -0400

    Commencement du form marde tits

---
+++ src/InfoClg/Adherents.java
@@ -0,0 +1,12 @@
+package InfoClg;
+
+import javax.swing.*;
+
+/**
+ * Created by 201332037 on 2015-04-13.
+ */
+public class Adherents {
+    private JButton ajouterButton;
+    private JButton modifierButton;
+    private JList list1;
+}
